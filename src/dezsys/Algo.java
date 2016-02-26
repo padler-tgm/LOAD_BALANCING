c0e9628011c45fce3f68922af943be2edf8496ed
Adin Karic <akaric@student.tgm.ac.at>
2016-02-26 11:33:36 +0100

    LeastConnection

--- src/dezsys/Algo.java
+++ src/dezsys/Algo.java
@@ -1,13 +1,46 @@
 package dezsys;
 
+import java.util.ArrayList;
+
 public class Algo {
 	//LIEFERT DIE IP DES SERVERS
 	//BENÖTIGT SERVER POLL MIT ALLEN IPS
-	public Algo(){
+	private ArrayList<Server> server;
+	public Algo(ArrayList<Server> server){
+		this.server = server;
+	}
+	
+	private String algoLeastCon() {
+		String ip = "";
+		int minAnz=999;
+		int ind=-1;
+		for(int i =0; i < server.size() ; i++){
+			if(minAnz > server.get(i).getAnzCon()){
+				minAnz = server.get(i).getAnzCon();
+				ind = i;
+			}
+		}
+		ip = server.get(ind).getIP();
+		//erhoehen des counters
+		server.get(ind).connect();
+		return ip;
 		
 	}
 	
-	public String getServer(){
-		return null;
+	private String algoWeightDist() {
+		return "";
+		
+	}
+	public String getServer(int art){
+		String ip="";
+		if(art == 1){
+			ip = this.algoLeastCon();
+		}else{
+			if(art == 2){
+				ip = this.algoWeightDist();
+			}
+		}
+		
+		return ip;
 	}
 }
--- src/dezsys/Server.java
+++ src/dezsys/Server.java
@@ -1,5 +1,27 @@
 package dezsys;
 
 public class Server {
-
+	private int anzCon;
+	private String ip;
+	
+	public Server(String ip,int anzCon) {
+		this.ip = ip;
+		this.anzCon=anzCon;
+	}
+	
+	public int getAnzCon(){
+		return anzCon;
+	}
+	
+	public String getIP(){
+		return ip;
+	}
+	
+	public void connect (){
+		this.anzCon+=1;
+	}
+	
+	public void disconnect (){
+		this.anzCon-=1;
+	}
 }
